96ec17d0975bba3886ed540947bda4e64333d8fb
Robert Hadaway <rhadaway14@gmail.com>
2022-01-20 22:22:54 -0700

    more cors

--- src/pages/launch.js
+++ src/pages/launch.js
@@ -5,6 +5,8 @@ import Signin from '../components/signin/index'
 
 export default function Launch() {
 
+    const header = new Headers;
+            header.append('Access-Control-Allow-Origin', 'https://master.d28iptyfs5v9n5.amplifyapp.com/')
 
     function get(){
         console.log('please')
@@ -14,7 +16,7 @@ export default function Launch() {
             <Signin>
                 <Signin.Card>
                     <Signin.Title>Launch Cluster</Signin.Title>
-                    <Signin.Button onClick={() => fetch('https://r4761bgkfa.execute-api.us-west-1.amazonaws.com/dev')}/>
+                    <Signin.Button onClick={() => fetch('https://r4761bgkfa.execute-api.us-west-1.amazonaws.com/dev', {headers: header})}/>
                 </Signin.Card>
             </Signin>
         </>
